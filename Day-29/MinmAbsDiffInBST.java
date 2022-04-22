380fe925c616bac3cf7fec3726da929d3ff239c6
kranuj3122 <kumaranuj22799@gmail.com>
2022-04-22 11:58:07 +0530

    30th commit

---
+++ Day-29/MinmAbsDiffInBST.java
@@ -0,0 +1,24 @@
+
+public class MinmAbsDiffInBST {
+    public int ans;
+    public int prev;
+    
+    public void inorder(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        inorder(root.left);
+        if(prev != -1 && root.val - prev < ans) {
+            ans = root.val - prev;
+        }
+        prev = root.val;
+        inorder(root.right);
+    }
+    
+    public int getMinimumDifference(TreeNode root) {
+        ans = Integer.MAX_VALUE;
+        prev = -1;
+        inorder(root);
+        return ans;
+    }
+}
